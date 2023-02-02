48544c4f300ce10cd2b1d1179e4dbde672db81f3
Azimut191 <azimut19111@gmail.com>
2023-02-02 21:38:19 +0200

    lesson12Object

--- js_lessons.js
+++ js_lessons.js
@@ -1,79 +1,71 @@
-let num = 0;
-while (num < 5) {
-   console.log(num);
-   num++;
+console.log ('Задача 1');
+const userInfo = {
+   name: "Вася",
+   age: 30,
 }
-console.log ('--------');
-let num1 = 5;
-while (num1) {
-   console.log(num1);
-   num1--;
+console.log ('Задача 2');
+let userInfo2 = {
+   name: "Вася",
+   age: 30,
+   "58": 'Значение свойства'
 } 
-console.log ("----------")
-let num3 = 0;
-for (; num3 <5; num3++) {
-   console.log(num3);
+console.log(userInfo2[58]);
+
+console.log ("Задача 3");
+let userInfo3 = {
+   name:"Вася",
+   age: 30,
 }
-console.log (`Вывод вне цикла: ${num3}`);
-console.log ("---Деректива стоп (break)----");
-let num4= 0;
-for (; num4 < 10; num4++) {
-   console.log(num4);
-   if (num4 == 7) break;
-}
-console.log(`Работа закончена потому что мы достиги нужного в условии значения ${num4}`);
-console.log ('---Деректива пропустить (continue)---');
-for (num = 0; num<5; num++) {
-   if (num==2) continue;
-   console.log(num);
-}
-console.log("-- Работа с метками цикла--")
-let num5 = 0;
-firstfor: for (; num5<5; num5++) {
-   for (size = 0; size <3; size++) {
-      if (size==5) {
-         break firstfor;
-      }
-      console.log(`${num5} ${size}`);
-   }
-}
-console.log ("ДОМАШКА");
-console.log ("Задача1");
-let dz =0;
-while (dz<5) {
-   console.log(dz);
-   dz++
-}
-while (dz) {
-   console.log(dz);
-   dz--
-}
-do {
-   console.log(dz);
-   // dz++;
-} while (dz==5);
-for (; dz<6; dz++) {
-   console.log (dz);
-}
-console.log ("Задача 2 - тут должно быть от 8-1");
-dz =8;
-while (dz) {
-   console.log(dz);
-   dz--;
-}
-console.log ('Задача 3');
-dz =0 
-while (dz<3){
-   console.log (`Число: ${dz}`);
-   dz++;
-}
+let user = userInfo3;
+user.age = 45;
+console.log(userInfo3.age);
+
 console.log("Задача 4");
-dz=0
-twofor: for (; dz<2; dz++){
-   for (size=0; size<3; size++) {
-     if (size==1) {
-      break twofor;
+let userInfo4 = {
+   name: "Вася",
+   age: 30,
+   showInfo (){
+      console.log (`${this.name}`);
    }
-   console.log(size);
 }
+let user4 = userInfo4;
+userInfo4 = null;
+user4.showInfo();
+
+console.log ('Задача 5');
+let userInfo5 = {
+   name:"вася",
+   age: 30,
 }
+for (const data in userInfo5) {
+   const value = userInfo5[data];
+   console.log(value);
+}
+console.log ("Задача 6");
+let userInfo6 = {
+   name: "Денис",
+   age: 36,
+   address: {
+      city:"Одесса",
+   }
+}
+for (const data in userInfo6) {
+   console.log (userInfo6.address.city);
+   console.log (userInfo6);
+}
+// console.log("Задача 7");
+// const userInfo7 = {
+//    name:"Денис",
+//    age: 36,
+//    "likes js": true, 
+// };
+// consol.log(userInfo7["likes js"]);
+console.log("задача 8");
+let userInfo8 = {};
+userInfo8.name ="Денис";
+userInfo8.age = 36;
+console.log (userInfo8);
+userInfo8.name = "лена";
+console.log (userInfo8);
+delete userInfo8.name;
+console.log (userInfo8);
